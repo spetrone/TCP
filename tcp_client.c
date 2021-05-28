f4455b03f201c3fdbf3f6e956f2909bd40f003c5
smpetrone <s-petrone@hotmail.com>
2021-05-28 12:00:08 -0700

    initial commit

---
+++ tcp_client.c
@@ -0,0 +1,8 @@
+/*
+ * Stephanie Petrone
+ * May 28, 2021
+ *
+ * Client program for TCP application
+ * */
+
+
---
+++ tcp_server.c
@@ -0,0 +1,6 @@
+/*
+ * Stephanie Petrone
+ * May 28, 2021
+ *
+ * Server program for TCP application 
+ * */
