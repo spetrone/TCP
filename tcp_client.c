7ffc18d7a35dc53c357d0558172dad438549772b
smpetrone <s-petrone@hotmail.com>
2021-05-28 20:28:20 -0700

    tcp_client and tcp_server implemented

--- tcp_client.c
+++ tcp_client.c
@@ -6,9 +6,97 @@
  * */
 
 #include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main()
+#define MAXLEN 128 //max size of message
+
+int main(int argc, char *argv[])
 {
-	printf("test");
-	return 0;
+	
+	int sockid; //socket to connect to server
+    struct sockaddr_in servAddr; //server address
+	unsigned short portNum; //used for getting command line port
+	char inputBuf[MAXLEN]; //buffer to sending strings
+	char recBuf[MAXLEN]; //buffer for receiving echo
+	int msgLength = 0; //length of message to send
+	int count = 0; //number of bytes with send() and recv()
+
+	//get command line arguments
+	if(argc != 2)
+	{
+		printf("Use: ./program PORT\n");
+	}
+
+ 	//get port from command line, turn into us
+	portNum = atoi(argv[1]);
+	memset(&servAddr, '\0', sizeof(servAddr));//initialize struct
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_port = htons(portNum);
+	servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	//create socket
+	if((sockid = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("Error with socket(): \n");
+		return -1;
+	}
+
+	//connect to server
+	if(connect(sockid, (struct sockaddr *)&servAddr, sizeof(servAddr)) != 0)
+	{
+		perror("Error with connect(): \n");
+		return -1;
+	}
+
+
+	//send message
+	printf("\nType a message to send to echo server, or -1 to quit...\n");
+	memset(inputBuf, '\0', sizeof(inputBuf)); //set buffer to 0
+	fgets(inputBuf, MAXLEN-1, stdin);
+	msgLength = strlen(inputBuf);
+	
+	//set sentinal value
+	if((atoi(inputBuf)) == -1)
+		msgLength = -1;
+	
+
+	//enter loop to send and receive
+	while(msgLength > 0)
+	{
+		if((count = send(sockid, inputBuf, msgLength, 0)) != msgLength)
+		{
+			perror("Error with send(): \n");
+			return -1;
+		}
+		else
+			printf("Sent message of length %d to server\n", count);
+
+		//receive echo
+		memset(recBuf, '\0', sizeof(recBuf)); //reset buffer
+		count = recv(sockid, recBuf, sizeof(recBuf), 0);
+
+		//print echo from server
+		printf("\n\nECHO from server: \n");
+		printf("length in bytes: %d\n", count);
+		printf("%s\n", recBuf);
+
+		//get new message or quit
+		printf("\nType a message to send to echo server, or -1 to quit...\n");
+		memset(inputBuf, '\0', sizeof(inputBuf)); //set buffer to 0
+		fgets(inputBuf, MAXLEN-1, stdin);
+		msgLength = strlen(inputBuf);
+		
+		//set sentinal value
+		if((atoi(inputBuf)) == -1)
+			msgLength = -1;
+
+	}
+
+	//close 
+ 	close(sockid); 
+
 };
--- tcp_server.c
+++ tcp_server.c
@@ -3,12 +3,114 @@
  * May 28, 2021
  *
  * Server program for TCP application 
+   iterative server
  * */
 
 #include <stdio.h>
+#include <stdio.h>
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAXLEN 128  //max byte size of message
+#define LISTEN_PORT 8989 //port TCP server will listen
+
 
 int main()
 {
-	printf("Hello world\n");
-	return 0;
+	struct sockaddr_in clientAddr; //holds client address of sock
+	int addrLen; //holds lenght of client adress
+	int count = 0 ; //bytes received wirth recv()
+	int connectSock; //new socket with connection
+	int listenSock; //socket for listen()
+	char buf[MAXLEN]; //buffer for receiving 
+	struct sockaddr_in serverSockAddr;
+	char IP_str[16]; //for holding string of IP addr (up tp 16 chars)
+	
+	//create socket
+	listenSock = socket(PF_INET, SOCK_STREAM, 0);
+
+    //set address
+	memset(&serverSockAddr, '0', sizeof(serverSockAddr));
+	serverSockAddr.sin_family = AF_INET;
+	serverSockAddr.sin_port = htons(LISTEN_PORT);
+	serverSockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+
+	//bind socket
+	if( bind(listenSock, (struct sockaddr* )&serverSockAddr, sizeof(serverSockAddr)) == -1)
+	{
+        perror("Error with bind(): ");
+		return -1;
+	}
+	else
+	{
+		//ip to string for printing
+		memset(IP_str, '\0', sizeof(IP_str));
+		strcpy(IP_str, inet_ntoa(serverSockAddr.sin_addr));
+		
+		printf("\nlistenSock binded to port %u\n", LISTEN_PORT);
+		printf("\nIP address: %s\n\n", IP_str); 
+	}
+
+
+	//listen for connection
+	if (listen(listenSock, 1) == -1)
+	{
+		perror("Error with listen(): ");
+		return -1;
+	}
+
+	//print status
+	printf("\nServer listening on port: %u...\n", LISTEN_PORT);
+
+    
+	//iteratively accept connection
+	for(;;)
+	{
+		//accept new connection
+		if(( connectSock = accept(listenSock,(struct sockaddr *) &clientAddr, &addrLen)) == -1)
+		{
+			perror("Error with accept(): ");
+			return -1;
+		}
+		else printf("\nSuccesfully connected to client socket\n\n");
+
+		//print client information
+
+		
+		//receive first message then loop for more receiving
+		memset(buf, '\0', sizeof(buf)); //clear buffer
+		if((count = recv(connectSock, buf, sizeof(buf), 0)) < 0)
+		{
+			perror("error with first recv(): ");
+			return -1;
+		}
+				
+		//loop for recv() more until nothing more is received
+		while (count > 0)
+		{
+			//print in server the message received
+			printf("\nMessage, size %d received into buf: \n %s\n\n",count, buf);
+
+			//echo messsage
+			if ((send(connectSock, buf, count, 0)) != count)
+			{
+				perror("Error with echo send(): ");
+			}
+			else
+				printf("Sent message of size %d to client\n", count);
+
+			//receive next message
+			memset(buf, '\0', sizeof(buf)); //reset buffer
+			count = recv(connectSock, buf, sizeof(buf), 0); 
+			
+			
+		}
+
+	//close client socket 
+	close(connectSock);
+	}
 }
